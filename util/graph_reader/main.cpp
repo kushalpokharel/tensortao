153621cdb519e234f59409bd7cdf3cda3331ac99
kushalpokharel <kusaalpokharel@gmail.com>
2023-11-23 01:13:24 +0545

    Add BFS for graph traversal

--- util/graph_reader/main.cpp
+++ util/graph_reader/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "graph.h"
+#include "queue"
 
 int main(int args, char **argv)
 {
@@ -16,21 +17,40 @@ int main(int args, char **argv)
 	*ginst = new graph
 	<long, long, int, long, long, char>
 	(beg_file,csr_file,weight_file);
-    
+
+    for(int i = 0; i < ginst->vert_count; i++)
+    {
+       int beg = ginst->beg_pos[i];
+       int end = ginst->beg_pos[i+1];
+       std::cout<<i<<"'s neighor list: ";
+       for(int j = beg; j < end; j++)
+           std::cout<<ginst->csr[j]<<" ";
+       std::cout<<"\n";
+    }
+
     //**
     //You can implement your single threaded graph algorithm here.
     //like BFS, SSSP, PageRank and etc.
-    
-    //for(int i = 0; i < ginst->vert_count+1; i++)
-    //{
-    //    int beg = ginst->beg_pos[i];
-    //    int end = ginst->beg_pos[i+1];
-    //    std::cout<<i<<"'s neighor list: ";
-    //    for(int j = beg; j < end; j++)
-    //        std::cout<<ginst->csr[j]<<" ";
-    //    std::cout<<"\n";
-    //} 
-     
+    std::queue<int> q;
+    bool visited[ginst->vert_count] = {false};
+    q.push(0);
+    visited[0] = true;
+
+    while(!q.empty()){
+        int i = q.front();
+        q.pop();
+        std::cout<<"Visited "<<i<<"\n";
+        int beg = ginst->beg_pos[i];
+        int end = ginst->beg_pos[i+1];
+        for(int j = beg; j < end; j++){
+            int neighbor = ginst->csr[j];
+            if(!visited[neighbor]){
+                q.push(neighbor);
+                visited[neighbor] = true;
+            }
+        }
+    }
+
 
 	return 0;	
 }
